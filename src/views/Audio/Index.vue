b0e4686ac53021d1f7b4b8a1f5c450f954dd73dd
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2019-11-16 12:52:56 +0100

    Hola

--- src/views/Audio/Index.vue
+++ src/views/Audio/Index.vue
@@ -3,10 +3,10 @@
     <MediaRecorderHeader />
 
     <div class="shadow-sm p-5 bg-white rounded">
-      <b-button variant="primary" class="mr-2">Grabar</b-button>
-      <b-button variant="outline-primary" class="mr-2">Pausar</b-button>
-      <b-button variant="outline-success" class="mr-2">Reanudar</b-button>
-      <b-button variant="secondary" class="mr-2">Parar</b-button>
+      <b-button variant="primary" class="mr-2" @click="start">Grabar</b-button>
+      <b-button variant="outline-primary" class="mr-2" @click="pause">Pausar</b-button>
+      <b-button variant="outline-success" class="mr-2" @click="resume">Reanudar</b-button>
+      <b-button variant="secondary" class="mr-2" @click="stop">Parar</b-button>
 
       <hr>
 
@@ -19,11 +19,18 @@
       </p>
 
       <!-- Si hay audios -->
-      <div>
+      <div v-if="hasAudios">
         <ul class="list-unstyled audio-list">
           <!-- TODO -->
           <!-- Bucle de Audios -->
-          <li></li>
+          <li v-for="(a, index) in audioList" :key="a.src">
+            <div class="d-flex justify-content-between mb-2">
+              <audio :src="a.src" controls class="w-100 mr-2"></audio>
+              <div class="d-flex align-content-center">
+                <b-button variant="danger" @click="deleteAudio(index)">Borrar</b-button>
+              </div>
+            </div>
+          </li>
         </ul>
       </div>
 
@@ -41,23 +48,76 @@ export default {
     return {
       // TODO
       // variables
+      mediaRecorder: undefined,
+      chunks: [],
+      audioList: []
     }
   },
   computed: {
     // TODO
     // Expresiones
+    hasAudios () {
+      return this.audioList.length > 0
+    }
   },
   methods: {
-    // TODO
-    // Funciones / Métodos
+    start () {
+      console.log('start')
+      this.mediaRecorder.start()
+    },
+    stop () {
+      console.log('stop')
+      this.mediaRecorder.stop()
+    },
+    pause () {
+      console.log('pause')
+      this.mediaRecorder.pause()
+    },
+    resume () {
+      console.log('resume')
+      this.mediaRecorder.resume()
+    },
+    deleteAudio (idx) {
+      this.audioList.splice(idx, 1)
+    }
   },
   created () {
     // Comprobar si el navegador soporta esta 'feature'
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-
       // TODO
       // Pedir acceso al micrófono
+      navigator.mediaDevices.getUserMedia({ audio: true })
+        .then((stream) => {
+          // MediaStream
+          console.log(stream)
 
+          // MediaRecorder
+          this.mediaRecorder = new MediaRecorder(stream)
+
+          // Cuando haya datos disponibles
+          this.mediaRecorder.ondataavailable = (e) => {
+            this.chunks.push(e.data)
+          }
+
+          // --
+          this.mediaRecorder.onstop = (e) => {
+            const blob = new Blob(this.chunks, { 'type': 'audio/ogg; codecs=opus' })
+
+            // Vaciamos los trozos
+            this.chunks = []
+            const audioURL = window.URL.createObjectURL(blob)
+
+            // Audios
+            this.audioList.push({ src: audioURL })
+
+            // Revocar
+            window.URL.revokeObjectURL(blob)
+          }
+        })
+        .catch((err) => {
+          console.log('Audio:')
+          console.log(err)
+        })
     } else {
       console.log('-- No audio 😭')
     }
